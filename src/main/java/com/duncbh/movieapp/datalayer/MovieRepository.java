76fa58c6aa1f803e9bc02d3b69ba402fa2e3e772
DuncanBH <2031593@champlaincollege.qc.ca>
2022-02-12 18:05:26 -0500

    Cleaned up MovieRepository.java

--- src/main/java/com/duncbh/movieapp/datalayer/MovieRepository.java
+++ src/main/java/com/duncbh/movieapp/datalayer/MovieRepository.java
@@ -1,10 +1,7 @@
 package com.duncbh.movieapp.datalayer;
 
-import com.duncbh.movieapp.presentationlayer.MovieRequestModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Integer> {
